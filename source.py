690ce05f4e447c81d6687248455cc8ca432c90de
g-codesteller <pallab.maji@gahanai.com>
2024-01-29 23:51:12 +0530

    first phase completed

---
+++ source.py
@@ -0,0 +1,69 @@
+'''
+ # @ Author: Pallab Maji
+ # @ Create Time: 2024-01-29 17:22:56
+ # @ Modified time: 2024-01-29 21:54:32
+ # @ Description: Enter description here
+ '''
+
+import os
+import cv2
+import numpy as np
+from segment_anything import sam_model_registry
+from utils import improc
+import torch
+
+
+
+def main(MODEL_TYPE, data_dir):
+    WORKSPACE = os.getcwd()
+    MODEL_DIR = os.path.join(WORKSPACE, "./models/pth")
+
+    SAM_MODEL_ARCH = "sam_vit_h_4b8939.pth"
+    SAM_HMODEL_PATH = os.path.join(MODEL_DIR, SAM_MODEL_ARCH)
+    DEVICE = 'cuda' if torch.cuda.is_available() else "cpu"
+
+    sam_model = sam_model_registry[MODEL_TYPE](checkpoint=SAM_HMODEL_PATH).to(device=DEVICE)
+
+    image_dir = os.path.join(data_dir, "NATT360JPEG")
+    anno_dir = os.path.join(data_dir, "NATT360JPEG_Annotated")
+
+    # Get Image List
+    image_list = improc.get_image_list(image_dir)  
+    print("Total Images: ", len(image_list))
+
+    # Get Annotation List
+    anno_list = improc.get_filepaths_list(anno_dir, ".txt", "classes.txt")
+    print("Total Annotations: ", len(anno_list))
+
+    # Get Image with Annotation
+    database = improc.get_image_with_annotation(image_list, anno_list)
+
+    print("Total Images with Annotation: ", len(database))
+
+    improc.create_segmentation_mask(database, sam_model)
+
+
+    # # Test bbox prompt
+    # image_path = list(database.keys())[0]
+    # anno_path, imsize = database[image_path]
+
+    # # Load & Preprocess image
+    # # image_rgb, image_bgr = improc.process_image(image_path, re_size=None)
+    # image_rgb, image_bgr = improc.process_image(image_path, re_size=(1200, 720))
+    # # Get bbox prompt
+    # bbox_prompt = improc.get_bbox_prompt(anno_path, imsize=(1200, 720))
+
+
+    # cv2.rectangle(image_bgr, (bbox_prompt[0], bbox_prompt[1]), (bbox_prompt[2], bbox_prompt[3]), (0, 255, 0), 2)
+    # cv2.imshow("Image", image_bgr)
+    # cv2.waitKey(0)
+
+
+if __name__ == "__main__":
+
+    data_dir = "/home/pallab/Dataset/telko-eyeware/Fotos_360_CRUDAS/NATT/"
+    MODEL_TYPE = "vit_h"
+    main(MODEL_TYPE, data_dir)
+
+
+
--- utils/improc.py
+++ utils/improc.py
@@ -7,6 +7,40 @@
  '''
 
 import cv2
+import os
+from segment_anything import SamPredictor
+import tqdm
+import numpy as np
+import gc
+import matplotlib.pyplot as plt
+import imagesize
+
+
+def show_mask(mask, ax, random_color=False):
+    print(mask.shape)
+    print(str(mask))
+
+    if random_color:
+        color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
+        print(str(color))
+    else:
+        color = np.array([30 / 255, 144 / 255, 255 / 255, 0.6])
+    h, w = mask.shape[-2:]
+    mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
+    ax.imshow(mask_image)
+    del mask
+    gc.collect()
+
+def show_masks_on_image(raw_image, masks):
+  plt.imshow(np.array(raw_image))
+  ax = plt.gca()
+  ax.set_autoscale_on(False)
+  for mask in masks:
+      show_mask(mask, ax=ax, random_color=True)
+  plt.axis("off")
+  plt.show()
+  del mask
+  gc.collect()
 
 
 def process_image(img_path, re_size=(1200, 720)):
@@ -17,3 +51,117 @@ def process_image(img_path, re_size=(1200, 720)):
     image_rgb = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2RGB)
 
     return image_rgb, image_bgr
+
+
+def get_bbox_prompt(anno_path, imsize=None):
+    '''
+    TODO: Implement Object Detector to get bbox prompt
+    Currently Reading from Annotation File
+    '''
+    with open(anno_path, "r") as f:
+        lines = f.readlines()
+        bbox_coords = lines[0].split(" ")[1:]
+        bbox_coords = [i for i in bbox_coords]
+        bbox_coords = [bbox_coords[0], bbox_coords[1], bbox_coords[2], bbox_coords[3]]
+
+        bbox_prompt = list(map(float, bbox_coords))
+        bbox_prompt[0] = bbox_prompt[0] - bbox_prompt[2] / 2
+        bbox_prompt[1] = bbox_prompt[1] - bbox_prompt[3] / 2
+        bbox_prompt[2] = bbox_prompt[0] + bbox_prompt[2] 
+        bbox_prompt[3] = bbox_prompt[1] + bbox_prompt[3] 
+
+        bbox_prompt = np.array(bbox_prompt)
+        if imsize is not None:
+            # print("Image Size: ", imsize)
+            # convert normalized coords to pixel coords
+            bbox_prompt[0] = bbox_prompt[0] * imsize[0]
+            bbox_prompt[1] = bbox_prompt[1] * imsize[1]
+            bbox_prompt[2] = bbox_prompt[2] * imsize[0]
+            bbox_prompt[3] = bbox_prompt[3] * imsize[1]
+            bbox_prompt = bbox_prompt.astype(int)
+            # print("Bbox Prompt: ", bbox_prompt)
+            # bbox_coords = bbox_coords.astype(int)
+    return bbox_prompt
+
+
+def get_image_list(image_dir):
+    image_list = []
+    for root, dirs, files in os.walk(image_dir):
+        for file in files:
+            if file.endswith(".jpeg"):
+                image_list.append(os.path.join(root, file))
+    return image_list
+
+
+def get_filepaths_list(input_dir, file_ext, exclude_file=None):
+    filepath_list = []
+    if file_ext[0] != ".":
+        file_ext = "." + file_ext
+
+    for root, dirs, files in os.walk(input_dir):
+        for file in files:
+            if exclude_file not in file and file.endswith(file_ext):
+                filepath_list.append(os.path.join(root, file))
+    return filepath_list
+
+
+def get_image_with_annotation(image_list, annotation_list):
+    '''
+    '''
+    database = {}
+    for image_path in image_list:
+        anno_path = image_path.replace("NATT360JPEG", "NATT360JPEG_Annotated").replace(".jpeg", ".txt")
+        if anno_path in annotation_list:
+            imsize = imagesize.get(image_path)
+            database[image_path] = [anno_path, imsize]
+        else:
+            print("Annotation not found for: ", image_path)
+    return database
+
+def center_crop(img, percentange=0.8):
+    '''
+    '''
+    h, w, c = img.shape
+    h_crop = int(h * percentange)
+    w_crop = int(w * percentange)
+    h_start = int((h - h_crop) / 2)
+    w_start = int((w - w_crop) / 2)
+    img_cropped = img[h_start:h_start+h_crop, w_start:w_start+w_crop, :]
+    return img_cropped
+
+
+
+def create_segmentation_mask(db, sam_model):
+
+    mask_predictor = SamPredictor(sam_model)
+
+    for image_path in tqdm.tqdm(db.keys()):
+        anno_path, imsize = db[image_path]
+        # Load & Preprocess image
+        image_rgb, image_bgr = process_image(image_path, re_size=None)
+        # Get bbox prompt
+        bbox_prompt = get_bbox_prompt(anno_path, imsize=imsize)
+
+        mask_predictor.set_image(image_rgb)
+        # Predict mask with bounding box prompt
+        masks, scores, logits = mask_predictor.predict(
+            box=bbox_prompt,
+            multimask_output=False
+            )  
+
+        # Multiply image with mask
+        masked_image = np.multiply(image_bgr, masks[0][..., None])
+
+        # Add white background
+        masked_image = masked_image + (1 - masks[0][..., None]) * 255
+
+        masked_image_cropped = center_crop(masked_image, percentange=0.75)
+
+        # cv2.imshow("Image", masked_image)
+        # cv2.waitKey(0)
+        # # Get the output path & write image
+        imwrite_path = image_path.replace("NATT360JPEG", "NATT360JPEG_Processed")
+        os.makedirs(os.path.dirname(imwrite_path), exist_ok=True)
+        cv2.imwrite(imwrite_path, masked_image_cropped)
+
+    return None
